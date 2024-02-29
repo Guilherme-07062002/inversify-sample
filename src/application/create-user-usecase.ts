c846d34ee7d548a4b76cfe7524a0b3013171abe0
Guilherme-07062002 <guilherme762002@gmail.com>
2024-02-29 17:10:52 -0300

    feat: inject user repository in create user usecase

--- src/application/create-user-usecase.ts
+++ src/application/create-user-usecase.ts
@@ -1,9 +1,15 @@
+import "reflect-metadata";
+import { inject, injectable } from "inversify";
 import { CreateUserDTO } from "../domain/dtos";
 import { Usecase } from "../domain/ports";
 import { UserRepository } from "../domain/repositories";
+import { REPOSITORY_TYPES } from "../domain/repositories";
 
+@injectable()
 export class CreateUserUsecase implements Usecase<CreateUserDTO, boolean> {
-  constructor(private readonly userRepository: UserRepository) { }
+  constructor(
+    @inject(REPOSITORY_TYPES.UserRepository) private readonly userRepository: UserRepository
+  ) { }
 
   async execute(data: CreateUserDTO): Promise<boolean> {
     const result = await this.userRepository.create(data);
