e44d36651d47a5bf30eaadb90127c3d9d6a0dfe8
Guilherme-07062002 <guilherme762002@gmail.com>
2024-02-29 17:12:43 -0300

    feat: inject create user usecase in create user controller

--- src/interface/create-user-controller.ts
+++ src/interface/create-user-controller.ts
@@ -1,15 +1,21 @@
+import 'reflect-metadata';
 import { CreateUserUsecase } from "../application";
 import { CreateUserDTO } from "../domain/dtos";
 import { Controller } from "../domain/ports/controller";
 import { Request, Response } from "../domain/ports/http";
 import { badRequest, ok } from "./adapters/http";
+import { USECASE_TYPES } from "../application";
+import { inject, injectable } from "inversify";
 
 namespace Request {
   export type Body = CreateUserDTO;
 }
 
+@injectable()
 export class CreateUserController implements Controller {
-  constructor(private readonly createUserUseCase: CreateUserUsecase) { }
+  constructor(
+    @inject(USECASE_TYPES.CreateUserUseCase) private readonly createUserUseCase: CreateUserUsecase
+  ) { }
 
   async handle(request: Request<Request.Body>): Promise<Response> {
     const result = await this.createUserUseCase.execute(request.body);
