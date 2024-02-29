d3bd49caedaf715534f4927990dc784faf68f481
Guilherme-07062002 <guilherme762002@gmail.com>
2024-02-29 17:13:01 -0300

    feat: set fake user repository as injectable

--- src/infra/fake-user-repository.ts
+++ src/infra/fake-user-repository.ts
@@ -1,6 +1,9 @@
+import 'reflect-metadata';
 import { CreateUserDTO } from "../domain/dtos";
 import { UserRepository } from "../domain/repositories";
+import { injectable } from 'inversify';
 
+@injectable()
 export class FakeUserRepository implements UserRepository {
   async create(data: CreateUserDTO): Promise<boolean> {
     console.log('new user created:', data);
